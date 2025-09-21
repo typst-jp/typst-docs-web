7618dd58a6900085ec0f268ef7a393f303a278a8
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2025-09-21 14:27:58 +0900

    chore: add project

---
+++ src/components/ui/common/index.ts
@@ -0,0 +1,9 @@
+export { Breadcrumbs, type BreadcrumbsProps } from "./Breadcrumbs";
+export { Footer } from "./Footer";
+export { Header } from "./Header";
+export { SearchWindow } from "./SearchWindow";
+export { SideNavigation, type SideNavigationProps } from "./SideNavigation";
+export { SiteNoticeBanner } from "./SiteNoticeBanner";
+export { SiteTitle } from "./SiteTitle";
+export { TableOfContents, type TableOfContentsProps } from "./TableOfContents";
+export { TranslationStatusAlert } from "./TranslationStatusAlert";
